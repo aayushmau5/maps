bdd87097ea3a61880245a4afef70f5da256a06a0
Aayush Kumar Sahu <aayushmau5@gmail.com>
2022-10-01 17:31:20 +0530

    add api url

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 export async function getDistance(origin, destination) {
   const encodedOrigin = encodeURIComponent(origin);
   const encodedDestination = encodeURIComponent(destination);
-  const endpoint = `http://localhost:8000/?origin=${encodedOrigin}&destination=${encodedDestination}`;
+  const endpoint = `${process.env.BACKEND_API_URL}?origin=${encodedOrigin}&destination=${encodedDestination}`;
 
   const response = await fetch(endpoint);
 
